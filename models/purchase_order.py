605c04bf55dd16f004c1d01f258f13a71840c36a
AfroMonkey <moisalejandro@gmail.com>
2017-07-26 17:02:56 -0500

    [IMP]

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -6,8 +6,7 @@ class PurchaseOrder(models.Model):
 
     def _get_addres(self):
         company = self.env['res.company'].browse(1)
-        return company.street + ' ' + company.l10n_mx_street3 + '\n' +
-        company.street2 + '\n' + company.state_id.name + ' ' + company.country_id.name + ' ' + company.zip
+        return company.street + ' ' + company.l10n_mx_street3 + '\n' + company.street2 + '\n' + company.state_id.name + ' ' + company.country_id.name + ' ' + company.zip
 
     new_dest = fields.Text(
         default=lambda self: self._get_addres(),
